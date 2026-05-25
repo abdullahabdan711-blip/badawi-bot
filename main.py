9bc9d3215267df576faad36cb786057ad9f936dc
abdullahabdan711-blip <abdullahabdan711@gmail.com>
2026-05-25 04:09:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ TOKEN = "8627412491:AAHF0yOhd_lzaK-imMtU_SpFuV8aw1KEEfQ"
 bot = TeleBot(TOKEN)
 
 # 3️⃣ إعداد مفتاح ذكاء جوجل جمناي (تم الحفظ بنجاح يا حمد ✅)
-GEMINI_KEY = "AIzaSyCcYSdEfx1yq0U6tKaGw-bxRjscEXwqb-k"
+GEMINI_KEY = "AIzaSyDZiFiZ2XDuHwYeqMOYVoBxqS3j1y9qVXU"
 genai.configure(api_key=GEMINI_KEY)
 model = genai.GenerativeModel('gemini-pro')
 
